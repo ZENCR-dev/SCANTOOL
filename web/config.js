9e840a88538f78251171635aae4915287fd9ef01
Renjie <info@zencr.org>
2026-07-17 21:25:18 +1200

    sheets URLs filled

--- web/config.js
+++ web/config.js
@@ -6,8 +6,8 @@ window.RECEIVING_CONFIG = {
   apiUrl:
     'https://script.google.com/macros/s/AKfycbzvvpkw0MhBBGsYPTut5e1G_QfMk3i5AsiObZ2DK50tq-rDMaxwZmRl-8g8AWCqc18/exec',
   batches: [
-    { name: '批次 1', sheetUrl: '' },
-    { name: '批次 2', sheetUrl: '' },
+    { name: '批次 1', sheetUrl: 'https://docs.google.com/spreadsheets/d/1yzZyLneoka48attHdTTgbodxF9gvg7ikj9CnRiSTRSg/edit?usp=sharing' },
+    { name: '批次 2', sheetUrl: 'https://docs.google.com/spreadsheets/d/1RVp19PziTpyfEDtAx3ID7ZegLhiNRVyOBztqpFNmS5g/edit?usp=sharing' },
     { name: '批次 3', sheetUrl: '' },
     { name: '批次 4', sheetUrl: '' },
   ],
